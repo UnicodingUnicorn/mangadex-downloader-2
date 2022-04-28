6179ed2940c1d6ee9e8bb6705123a2f01b8f7bd7
Daniel Lim <7555ic@gmail.com>
2022-04-28 15:38:47 +0100

    feat: chapter image downloading

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ mod types;
 
 #[tokio::main]
 async fn main() {
-    let manga = manga::Manga::new("https://mangadex.org/title/348966d0-c807-45cf-9260-8adf006a9da6/kono-bijutsubu-ni-wa-mondai-ga-aru", "en").await.unwrap();
-    println!("{:#?}", manga);
+    let mut manga = manga::Manga::new("https://mangadex.org/title/348966d0-c807-45cf-9260-8adf006a9da6/kono-bijutsubu-ni-wa-mondai-ga-aru", "en").await.unwrap();
+    let _ = manga.populate_chapters("en").await.unwrap();
+    let _ = manga.download_chapters().await.unwrap();
+
+    // println!("{:#?}", manga.chapters);
 }
--- src/manga.rs
+++ src/manga.rs
@@ -1,6 +1,11 @@
 use crate::requester::{ RateLimitedRequester, RequesterError };
 use crate::types::{ ChapterData, ChapterDataResponse, ChapterImageResponse, MangaDataResponse };
 
+use std::path::Path;
+use std::fs::{ self, File };
+use std::io::Write;
+use std::time::Duration;
+
 use regex::Regex;
 use thiserror::Error;
 
@@ -12,11 +17,28 @@ pub enum ChapterError {
     Requester(#[from] RequesterError),
 }
 
+#[derive(Debug, Error)]
+pub enum ChapterDownloadError {
+    #[error("requester error: {0}")]
+    Requester(#[from] RequesterError),
+    #[error("decoding error: {0}")]
+    Decode(#[from] reqwest::Error),
+    #[error("image has no content type")]
+    NoContentType,
+    #[error("illegible image mime type")]
+    IllegibleMime(#[from] reqwest::header::ToStrError),
+    #[error("unknown image mime type")]
+    Mime,
+    #[error("error saving image: {0}")]
+    IO(#[from] std::io::Error),
+}
+
 #[derive(Debug)]
 pub struct Chapter {
     volume: String,
     chapter: String,
     language: String,
+    base_url: String,
     urls: Vec<String>
 }
 impl Chapter {
@@ -28,13 +50,14 @@ impl Chapter {
             .await?;
 
         let urls = res.chapter.data.iter()
-            .map(|datum| format!("{}/data/{}/{}", res.base_url, res.chapter.hash, datum))
+            .map(|datum| format!("/data/{}/{}", res.chapter.hash, datum))
             .collect::<Vec<String>>();
 
         Ok(Self {
             volume: raw.attributes.volume.clone(),
             chapter: raw.attributes.chapter.clone(),
             language: raw.attributes.language.clone(),
+            base_url: res.base_url,
             urls,
         })
     }
@@ -69,6 +92,48 @@ impl Chapter {
 
         Ok(chapters)
     }
+
+    pub fn get_volume(&self) -> String {
+        match self.volume.parse::<f64>() {
+            Ok(v) => format!("Volume {}", v),
+            Err(_) => self.volume.clone(),
+        }
+    }
+
+    pub fn get_chapter(&self) -> String {
+        match self.chapter.parse::<f64>() {
+            Ok(c) => format!("Chapter {}", c),
+            Err(_) => self.chapter.clone(),
+        }
+    }
+
+    pub async fn download_to_folder(&self, requester:&mut RateLimitedRequester, master_directory:&str) -> Result<(), ChapterDownloadError> {
+        let _ = requester.insert_source(&self.base_url, &self.base_url, Duration::from_millis(100)); // Ignore conflicting aliases
+        let master_path = Path::new(master_directory).join(Path::new(&format!("{}/{}", self.get_volume(), self.get_chapter())));
+        fs::create_dir_all(&master_path)?;
+
+        for (i, url) in self.urls.iter().enumerate() {
+            let res = requester.request(&self.base_url, &url).await?;
+            let content_type = res.headers().get("Content-Type")
+                .ok_or(ChapterDownloadError::NoContentType)?
+                .to_str()?;
+
+            let extension = mime_guess::get_mime_extensions_str(content_type)
+                .ok_or(ChapterDownloadError::Mime)?
+                .iter().map(|s| *s)
+                .next()
+                .ok_or(ChapterDownloadError::Mime)?;
+
+            let body = res.bytes().await?;
+
+            // I'm too lazy to do async file io
+            let path = master_path.join(Path::new(&format!("{}.{}", i + 1, extension)));
+            let mut file = File::create(path)?;
+            let _ = file.write_all(&body)?;
+        }
+
+        Ok(())
+    }
 }
 
 
@@ -84,6 +149,8 @@ pub enum MangaError {
     Requester(#[from] RequesterError),
     #[error("error retrieving chapter data: {0}")]
     Chapter(#[from] ChapterError),
+    #[error("error downloading chapter: {0}")]
+    ChapterDownload(#[from] ChapterDownloadError),
 }
 
 fn get_id(url:&str) -> Option<String> {
@@ -95,11 +162,12 @@ fn get_id(url:&str) -> Option<String> {
     Some(id)
 }
 
-#[derive(Debug)]
 pub struct Manga {
+    pub id: String,
     pub title: String,
     pub available_languages: Vec<String>,
-    pub chapters: Vec<Chapter>,
+    pub chapters: Option<Vec<Chapter>>,
+    requester: RateLimitedRequester,
 }
 impl Manga {
     pub async fn new(url:&str, preferred_language:&str) -> Result<Self, MangaError> {
@@ -118,12 +186,27 @@ impl Manga {
             },
         };
 
-        let chapters = Chapter::get_all(&mut requester, &id, preferred_language).await?;
-
         Ok(Self{
+            id,
             title,
             available_languages: raw_manga_data.data.attributes.available_languages,
-            chapters,
+            chapters: None,
+            requester,
         })
+    }
+
+    pub async fn populate_chapters(&mut self, preferred_language:&str) -> Result<(), MangaError> {
+        let chapters = Chapter::get_all(&mut self.requester, &self.id, preferred_language).await?;
+        self.chapters = Some(chapters);
+
+        Ok(())
+    }
+
+    pub async fn download_chapters(&mut self) -> Result<(), MangaError> {
+        if let Some(chapter) = self.chapters.as_ref().unwrap().iter().next() {
+            let _ = chapter.download_to_folder(&mut self.requester, "").await?;
+        }
+
+        Ok(())
     }
 }
