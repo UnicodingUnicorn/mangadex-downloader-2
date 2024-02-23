1d5163e07796a111ea395b7a2e07d9d49c1f2586
Daniel Lim <orcas@octalorca.me>
2024-02-23 09:27:16 +0800

    fix: added required User-Agent header to requests

--- src/requester.rs
+++ src/requester.rs
@@ -90,7 +90,7 @@ impl RateLimitedRequester {
             None => "",
         };
 
-        let mut req = self.client.get(format!("{}{}", base_url, path));
+        let mut req = self.client.get(format!("{}{}", base_url, path)).header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0");
         if let Some(s) = source {
             req = req.header("Host", &s.host);
         }
